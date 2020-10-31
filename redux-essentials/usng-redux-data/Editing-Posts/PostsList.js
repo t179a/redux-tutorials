20b62eeef01db9a1800866d36a959fb43e9a0777
t179a <67852630+t179a@users.noreply.github.com>
2020-10-31 16:09:24 +0900

    Create PostsList.js

---
+++ redux-essentials/usng-redux-data/Editing-Posts/PostsList.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { PostAuthor } from './PostAuthor'
+
+export const PostsList = () => {
+  const posts = useSelector(state => state.posts)
+
+  const renderedPosts = posts.map(post => (
+    <article className="post-exerpt" key={post.id}>
+      <h3>{post.title}</h3>
+      <div>
+        <PostAuthor userId={post.user} />
+      </div>
+      <p className="post-content">{post.content.substring(0, 100)}</p>
+      <Link to={`/posts/${post.id}`} className="button muted-button">
+        View Post
+      </Link>
+    </article>
+  ))
+
+  return (
+    <section className="posts-list">
+      <h2>Posts</h2>
+      {renderedPosts}
+    </section>
+  )
+}
